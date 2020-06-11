a855b5e12ae35620c430c2a6216dcebbe4e48d82
annigue <a.gue@gmx.net>
2020-06-11 16:10:58 +0200

    Created attractions and funcionality for adding new attractions

--- BoredlessTourist.py
+++ BoredlessTourist.py
@@ -6,7 +6,7 @@ def get_destination_index(destination):
   destination_index = destinations.index(destination)
   return destination_index
 
-print(get_destination_index("Los Angeles, USA"))
+#print(get_destination_index("Bahamas"))
 
 
 def get_traveler_location(traveler):
@@ -16,3 +16,21 @@ def get_traveler_location(traveler):
 
 test_destination_index = get_traveler_location(test_traveler)
 print(test_destination_index)
+
+# defining a list of attractions
+attractions = [[] for i in destinations]
+
+print(attractions)
+
+def add_attraction(destination, attraction):
+  destination_index = destinations.index(destination)
+  try:
+    destination_index = destinations.index(destination)
+  except SyntaxError:
+    return destination_index
+  attractions_for_destinations = attraction.append[0]
+  return attractions_for_destinations
+
+
+
+print(add_attraction('Los Angeles, USA', ['Venice Beach', ['beach']]))
