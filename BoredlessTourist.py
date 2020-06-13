1433c1cb331b76ce2211ff424e689a3cc135d4eb
annigue <a.gue@gmx.net>
2020-06-13 16:44:31 +0200

    find attractions still doesnt work

--- BoredlessTourist.py
+++ BoredlessTourist.py
@@ -6,16 +6,12 @@ def get_destination_index(destination):
   destination_index = destinations.index(destination)
   return destination_index
 
-#print(get_destination_index("Bahamas"))
-
 
 def get_traveler_location(traveler):
   traveler_destination = traveler[1]
   traveler_destination_index = traveler.index(traveler_destination)
   return traveler_destination_index
 
-test_destination_index = get_traveler_location(test_traveler)
-print(test_destination_index)
 
 # defining a list of attractions
 attractions = [[] for i in destinations]
@@ -23,14 +19,38 @@ attractions = [[] for i in destinations]
 print(attractions)
 
 def add_attraction(destination, attraction):
-  destination_index = destinations.index(destination)
+  global destination_index
   try:
-    destination_index = destinations.index(destination)
-  except SyntaxError:
+    destination_index = get_destination_index(destination)
+  except:
     return destination_index
-  attractions_for_destinations = attraction.append[0]
-  return attractions_for_destinations
+  attractions.insert(destination_index,attraction)
+  return attractions
 
+add_attraction("Paris, France", ["the Louvre", ["art", "museum"]])
+add_attraction("Paris, France", ["Arc de Triomphe", ["historical site", "monument"]])
+add_attraction("Shanghai, China", ["Yu Garden", ["garden", "historcical site"]])
+add_attraction("Shanghai, China", ["Yuz Museum", ["art", "museum"]])
+add_attraction("Shanghai, China", ["Oriental Pearl Tower", ["skyscraper", "viewing deck"]])
+add_attraction("Los Angeles, USA", ["LACMA", ["art", "museum"]])
+add_attraction("São Paulo, Brazil", ["São Paulo Zoo", ["zoo"]])
+add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]])
+add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
+add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
 
+def find_attractions(destination, interests):
+  destination_index = get_destination_index(destination)
+  attractions_in_city = attractions[destination_index]
+  attractions_with_interest = []
+  for i in attractions_in_city:
+     possible_attraction = i
+     attractions_tags = i[1]
+     for interest in interests:
+        if (interest in attractions_tags):
+          attractions_with_interest.append(possible_attraction)
+  return attractions_with_interest
 
-print(add_attraction('Los Angeles, USA', ['Venice Beach', ['beach']]))
+#comment
+
+la_arts = find_attractions("Los Angeles, USA", ["art"])
+print(la_arts)
