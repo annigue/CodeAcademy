1fd321f1cdef8727f0c21ed7b0ca96b4a6605317
annigue <a.gue@gmx.net>
2020-06-23 17:27:17 +0200

    Added function to generate message for traveler and present attractions they might be interested in.

--- BoredlessTourist.py
+++ BoredlessTourist.py
@@ -18,15 +18,7 @@ attractions = [[] for i in destinations]
 
 #print(attractions)
 
-#def add_attraction(destination, attraction):
-#  global destination_index
- # try:
-  #  destination_index = get_destination_index(destination)
-  #except:
-  #  return destination_index
- # attractions.insert(destination_index,attraction)
-  #return(attractions)
-  
+ 
 def add_attraction(destination, attraction):
  destination_index = get_destination_index(destination)
  try:
@@ -60,6 +52,20 @@ def find_attractions(destination, interests):
       if interest in attractions_tags:
       	attractions_with_interest.append(possible_attraction[0])
   return(attractions_with_interest)
+  
+def get_attractions_for_traveler(traveler):
+	 traveler_destination = traveler[1]
+	 traveler_interests = traveler[2]
+	 traveler_attraction = find_attractions(traveler_destination, traveler_interests)
+	 interests_string = str("Hi "+ traveler[0] + ", we think you'll like these places around ")
+	 for each in traveler_attraction:
+	 	interests_string = interests_string + traveler_destination + str(": ") + str(each)
+	 return(interests_string)
+	 
+smills_france = get_attractions_for_traveler(['Dereck Smill', 'Paris, France', ['monument']])
+print(smills_france)
+ 
+ 
 
 la_arts = find_attractions("Los Angeles, USA", ['art'])
-print(la_arts)
+#print(la_arts)
